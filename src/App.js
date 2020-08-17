ca673e0849742df66d3077f33f8fb708271f0604
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2020-08-17 23:57:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+import { fetchData } from "./api";
+
+import coronaLogo from './images/covid-logo.png'
+
+class App extends React.Component {
+
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({data: fetchedData});
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country:country });
+  }
+
+  render() {
+
+    const {data, country} = this.state;
+    return (
+      <div className={styles.container}>
+
+        <img className={styles.image} src={coronaLogo} alt="Covid-19"/>
+        
+        <Cards data={data} />
+        <CountryPicker  handleCountryChange ={this.handleCountryChange} />
+        <Chart data= {data} country={country}/>
+
+        <h3><a href="https://github.com/Viraaaj/Corona_Tracker">Viraj/Covid-19 Tracker</a></h3>
+
+      </div>
+
+    )
+  }
+}
+
+export default App;
