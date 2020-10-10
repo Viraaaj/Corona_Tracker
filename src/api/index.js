54cc4700467a99f846be0beb9555cd146c7e4f94
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2020-10-10 13:02:56 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
+// const url = 'https://disease.sh/v3/covid-19/all';
+
 export const fetchData = async (country) => {
   let changeableUrl = url;
 
@@ -15,25 +17,19 @@ export const fetchData = async (country) => {
     return { confirmed, recovered, deaths, lastUpdate };
   }
   catch (error){
-    console.log(error);
+    return error;
   }
 }
 
 export const fetchDailyData = async () => {
-  try{
-    const {data} = await axios.get(`${url}/daily`);
-    
-    const modifiedData = data.map((dailyData) => ({
-      confirmed: dailyData.confirmed.total,
-      deaths: dailyData.deaths.total,
-      date: dailyData.reportDate,
-    }));
-    return modifiedData;
-  }
-  catch(error){
-    console.log(error);
-  }
-}
+    try {
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+
+      return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+      return error;
+    }
+  };
 
 export const fetchCountries = async () => {
   try{
@@ -41,7 +37,7 @@ export const fetchCountries = async () => {
     return countries.map((country) => country.name);
   }
   catch(error){
-    console.log(error);
+    return error;
   }
 
 }
